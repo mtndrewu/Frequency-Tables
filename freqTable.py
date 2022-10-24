7baca0d7a6a70e66348c771c3817cd043dcf70bc
Drew Urenko <drewurenko@gmail.com>
2022-10-24 18:19:12 -0400

    more pythonic

--- freqTable.py
+++ freqTable.py
@@ -1,69 +1,69 @@
 import pandas as pd
 
-def readData():   
+def read_data():   
     try:
-        fileName = input("Enter a file name: ")
-        inFile = open(fileName, 'r')    
+        file_name = input("Enter a file name: ")
+        in_file = open(file_name, 'r')    
     except FileNotFoundError:
         print("File not found.")
-        fileName = input("Enter a file name: ")
-        inFile = open(fileName, 'r')   
-    for line in inFile:
+        file_name = input("Enter a file name: ")
+        in_file = open(file_name, 'r')   
+    for line in in_file:
         strip = line.strip('\n')
         data = strip.split(', ')
     data = [int(num) for num in data]
     return data
 
-def classWidth(data, numClasses):
-    return int((max(data) - min(data)) / numClasses) + 1
+def calc_class_width(data, num_classes):
+    return int((max(data) - min(data)) / num_classes) + 1
 
-def lowerClassLimit(data, clsWidth, i):
-    return min(data) + (clsWidth * i)
+def calc_lc_limit(data, cls_width, i):
+    return min(data) + (cls_width * i)
 
-def upperClassLimit(data, clsWidth, i):
-    return min(data) + (clsWidth * (i+1)) - 1
+def calc_uc_limit(data, cls_width, i):
+    return min(data) + (cls_width * (i+1)) - 1
 
-def lowerClassBoundary(lcLimit):
-    return lcLimit - 0.5
+def calc_lc_boundary(lc_limit):
+    return lc_limit - 0.5
 
-def upperClassBoundary(ucLimit):
-    return ucLimit + 0.5
+def calc_uc_boundary(uc_limit):
+    return uc_limit + 0.5
 
-def classMidpoint(lcLimit, ucLimit):
-    return (lcLimit + ucLimit) / 2
+def calc_class_midpoint(lc_limit, uc_limit):
+    return (lc_limit + uc_limit) / 2
 
-def classFrequency(data, lcLimit, ucLimit):
+def calc_class_frequency(data, lc_limit, uc_limit):
     freq = 0
     for num in data:
-        if num in range(lcLimit, ucLimit + 1):
+        if num in range(lc_limit, uc_limit + 1):
             freq += 1
     return freq
 
-def cumulativeFrequency(freqs):
+def calc_cumulative_frequency(freqs):
     freq = 0
-    for clsFreq in freqs:
-        freq += clsFreq
+    for class_freq in freqs:
+        freq += class_freq
     return freq
     
-def relativeFrequency(freqs, i, data):
+def calc_relative_frequency(freqs, i, data):
     return round(freqs[i] / len(data), 3)
 
-def cumRelatFrequency(rel_freqs):
+def calc_cumulative_relative_frequency(rel_freqs):
     freq = 0
     for relatFreq in rel_freqs:
         freq += relatFreq
     return round(freq, 3)
 
 def main():
-    data = readData()
+    data = read_data()
     print(f"Data Set:\n{data}")
     print('')
     print(f'Largest num: {max(data)}\nSmallest num: {min(data)}')
     print('')
-    numClasses = int(input("How many classes are there? "))
-    clsWidth = classWidth(data, numClasses)
+    num_classes = int(input("How many classes are there? "))
+    class_width = calc_class_width(data, num_classes)
     print('')
-    print(f"Class Width: {clsWidth}")
+    print(f"Class Width: {class_width}")
     print('')
     columns = {
         "Class Limits (Lower-Upper)": [],
@@ -76,23 +76,23 @@ def main():
     }
     freqs = columns["Frequencies"]
     rel_freqs = columns["Relative Frequencies"]
-    for i in range(numClasses):
-        lcLimit = lowerClassLimit(data, clsWidth, i)
-        ucLimit = upperClassLimit(data, clsWidth, i)
-        columns["Class Limits (Lower-Upper)"] += [f"{lcLimit}-{ucLimit}"]
-        lcBound = lowerClassBoundary(lcLimit)
-        ucBound = upperClassBoundary(ucLimit)
-        columns["Class Boundaries (Lower-Upper)"] += [f"{lcBound}-{ucBound}"]
-        clsMidpoint = classMidpoint(lcLimit, ucLimit)
-        columns["Class Midpoints"] += [clsMidpoint]
-        clsFreq = classFrequency(data, lcLimit, ucLimit)
-        columns["Frequencies"] += [clsFreq]
-        cumFreq = cumulativeFrequency(freqs)
-        columns["Cumulative Frequencies"] += [cumFreq]
-        relatFreq = relativeFrequency(freqs, i, data)
-        columns["Relative Frequencies"] += [relatFreq]
-        cumRelatFreq = cumRelatFrequency(rel_freqs)
-        columns["Cumulative Relative Frequencies"] += [cumRelatFreq]
+    for i in range(num_classes):
+        lc_limit = calc_lc_limit(data, class_width, i)
+        uc_limit = calc_uc_limit(data, class_width, i)
+        columns["Class Limits (Lower-Upper)"] += [f"{lc_limit}-{uc_limit}"]
+        lc_boundary = calc_lc_boundary(lc_limit)
+        uc_boundary = calc_uc_boundary(uc_limit)
+        columns["Class Boundaries (Lower-Upper)"] += [f"{lc_boundary}-{uc_boundary}"]
+        class_midpoint = calc_class_midpoint(lc_limit, uc_limit)
+        columns["Class Midpoints"] += [class_midpoint]
+        class_frequency = calc_class_frequency(data, lc_limit, uc_limit)
+        columns["Frequencies"] += [class_frequency]
+        cumulative_frequency = calc_cumulative_frequency(freqs)
+        columns["Cumulative Frequencies"] += [cumulative_frequency]
+        relative_frequency = calc_relative_frequency(freqs, i, data)
+        columns["Relative Frequencies"] += [relative_frequency]
+        cum_rel_frequency = calc_cumulative_relative_frequency(rel_freqs)
+        columns["Cumulative Relative Frequencies"] += [cum_rel_frequency]
     df = pd.DataFrame(columns)
     print(df)
 
